ad5d7cbd9b121cc6f56106330f739c46fb04e7ca
sdf8057 <54014465+sdf8057@users.noreply.github.com>
2023-01-27 14:56:44 +0800

    luci-app-vssr: sync with upstream source

--- applications/luci-app-vssr/luasrc/controller/vssr.lua
+++ applications/luci-app-vssr/luasrc/controller/vssr.lua
@@ -10,6 +10,7 @@ function index()
 
 	local page = entry({'admin', 'services', 'vssr'}, alias('admin', 'services', 'vssr', 'client'), _('Hello World'), 0) -- 首页
 	page.dependent = true
+	page.acl_depends = { 'luci-app-vssr' }
 
 	entry({'admin', 'services', 'vssr', 'client'}, cbi('vssr/client'), _('SSR Client'), 10).leaf = true -- 基本设置
 	entry({'admin', 'services', 'vssr', 'servers'}, cbi('vssr/servers'), _('Severs Nodes'), 11).leaf = true -- 服务器节点
