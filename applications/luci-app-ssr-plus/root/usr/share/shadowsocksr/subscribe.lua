f4335d9f63de537066cd88c718b18c99ba5b11e7
sdf8057 <54014465+sdf8057@users.noreply.github.com>
2023-03-20 23:30:29 +0800

    luci-app-ssr-plus: subscribe: fix parse vmess link
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>
    (cherry picked from commit b0da2be4a61f371ef3d1d0a042806f64af3d29c4)

--- applications/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
+++ applications/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
@@ -224,7 +224,7 @@ local function processData(szType, content)
 		end
 		if info.tls == "tls" or info.tls == "1" then
 			result.tls = "1"
-			if info.host then
+			if info.sni and info.sni ~= "" then
 				result.tls_host = info.host
 			elseif info.sni then
 				result.tls_host = info.sni
